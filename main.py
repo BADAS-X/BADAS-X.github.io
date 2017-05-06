4067368dc925f89aac2cffb09c4ffd618f71762a
ManifoldFR <wilson.jallet@gmail.com>
2017-05-06 15:25:57 +0200

    update

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import markdown
 import glob
-from flask import Flask, request, session, escape, abort
+from flask import Flask, request, escape, abort
 from flask import Markup
 from flask import url_for, render_template, redirect
-from flask_login import LoginManager
 from io import BytesIO
+from flask_cas import CAS, login_required
 
 app = Flask(__name__)
+cas = CAS(app)
+app.config['CAS_SERVER'] = "https://cas.binets.fr/"
+app.config['CAS_AFTER_LOGIN'] = 'index'
+app.config['CAS_LOGIN_ROUTE'] = '/cas'
+app.config['CAS_LOGOUT_ROUTE'] = '/cas/logout'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 import sqlite3
 app.config.from_object(__name__)
@@ -22,9 +25,8 @@ def index():
     aboutfile = open('about.md','r',encoding='utf-8')
     about = Markup(markdown.markdown(text=aboutfile.read()))
 
-    postsFiles = glob.glob('posts/*.md')
     posts = []
-    for fil in postsFiles:
+    for fil in glob.glob('posts/*.md'):
         posts.append(
             Markup(markdown.markdown(
             text= open(fil, 'r',encoding='utf-8').read()
@@ -34,7 +36,6 @@ def index():
 
 
     return render_template(tmpl, about=about, posts=posts)
-
 
 app.debug = True
 
